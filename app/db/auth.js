539ce1a18c15b1c0fbad813827e5f97d82b569c6
maciejors <maciej.orslowski@gmail.com>
2024-01-28 18:38:47 +0100

    add auth functions

--- app/db/auth.js
+++ app/db/auth.js
@@ -1,4 +1,41 @@
-import { getAuth } from 'firebase/auth';
+import {
+	getAuth,
+	User,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from 'firebase/auth';
 import { app } from './firebase-setup';
 
 const auth = getAuth(app);
+
+/**
+ * This will raise an error when user's creation fails (user already exists or invalid password)
+ * @param {string} email
+ * @param {string} password
+ * @returns {User}
+ */
+export async function createUser(email, password) {
+	// this line will throw an error when user's creation fails
+	const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+	return userCredentials.user;
+}
+
+/**
+ * This will raise an error when sign-in fails (user does not exist or invalid password)
+ * @param {string} email
+ * @param {string} password
+ * @returns {User}
+ */
+export async function signIn(email, password) {
+	// this line will throw an error when sign-in fails
+	const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+	return userCredentials.user;
+}
+
+/**
+ * @param {User} user
+ * @returns {Promise<boolean>}
+ */
+export async function isUserDeveloper(user) {
+	return false;
+}
