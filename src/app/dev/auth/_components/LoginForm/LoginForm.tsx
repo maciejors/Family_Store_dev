8986bedafb4e3def1049befab45c5df8f2494009
maciejors <maciej.orslowski@gmail.com>
2026-01-10 18:16:18 +0100

    [feature/#35-improve-forms-styles]

--- src/app/dev/auth/_components/LoginForm/LoginForm.tsx
+++ src/app/dev/auth/_components/LoginForm/LoginForm.tsx
@@ -27,7 +27,7 @@ export default function LoginForm() {
 	}
 
 	return (
-		<form className="flex flex-col" onSubmit={handleSubmit(onSubmitValid)}>
+		<form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmitValid)}>
 			<h3>Logowanie</h3>
 			<TextInput
 				{...formRegister('email')}
