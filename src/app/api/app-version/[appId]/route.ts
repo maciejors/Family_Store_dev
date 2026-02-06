a8da7b418a368707e9eee74611e2696483663dc1
maciejors <maciej.orslowski@gmail.com>
2026-02-06 20:32:16 +0100

    [feature/#103] Add app URL to get app version response

--- src/app/api/app-version/[appId]/route.ts
+++ src/app/api/app-version/[appId]/route.ts
@@ -1,9 +1,13 @@
-import { NextRequest, NextResponse } from 'next/server';
 import { getCurrentAppVersion } from '@/lib/supabase/database/apps';
+import { NextRequest, NextResponse } from 'next/server';
 
-type VersionResponse = {
-	version?: string;
-	error?: string;
+type SuccessfulResponse = {
+	version: string;
+	url: string;
+};
+
+type ErrorResponse = {
+	error: string;
 };
 
 export async function GET(
@@ -14,26 +18,26 @@ export async function GET(
 	const appId = parseInt(appIdString);
 
 	if (isNaN(appId)) {
-		return NextResponse.json<VersionResponse>(
-			{ error: 'Invalid appId' },
-			{ status: 400 }
-		);
+		return NextResponse.json<ErrorResponse>({ error: 'Invalid appId' }, { status: 400 });
 	}
 
 	try {
 		const version = await getCurrentAppVersion(appId);
 
 		if (version === null) {
-			return NextResponse.json<VersionResponse>(
+			return NextResponse.json<ErrorResponse>(
 				{ error: 'App with the provided ID does not exist' },
 				{ status: 404 }
 			);
 		}
-		return NextResponse.json<VersionResponse>({ version }, { status: 200 });
+		return NextResponse.json<SuccessfulResponse>(
+			{ version, url: `https://family-store.vercel.app/apps/${appId}` },
+			{ status: 200 }
+		);
 	} catch (err) {
 		console.log(err);
 
-		return NextResponse.json<VersionResponse>(
+		return NextResponse.json<ErrorResponse>(
 			{ error: 'Failed to fetch data' },
 			{ status: 500 }
 		);
